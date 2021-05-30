6bfce1070366de2659af6c7986e01234c674bf3c
hi0711 <admin@hi0711.xyz>
2021-05-30 22:33:48 +0900

    feat: 🎸 instagram api データ取得
    ✅ Closes: #57

--- src_client/get-instagram.js
+++ src_client/get-instagram.js
@@ -1,6 +1,33 @@
-export default function getInstagram() {
+import axios from 'axios';
+
+// 環境変数から取得
+const TOKEN = process.env.INSTAGRAM_ACCESS_TOKEN;
+const BUSINESS_ID = process.env.BUSINESS_ID;
+
+// hashtag "instagood"のid
+// curl "https://graph.facebook.com/v10.0/ig_hashtag_search?user_id=${id}&q=instagood&access_token=${token}" で取得
+const hashId = '17843862859017769';
+
+// 検索フィールド定数化
+const searchFields = 'id,media_url';
+
+// 表示数
+const quantity = 6;
+
+// 生成したURL
+const graphApiUrl = `https://graph.facebook.com/v10.0/${hashId}/recent_media?user_id=${BUSINESS_ID}&access_token=${TOKEN}&fields=${searchFields}&limit=${quantity}`;
+
+
+export const fetchInstagram = () => {
     const target = document.querySelector('.bl_instagram_content');
     if (target) {
-
+        const getData = async () => {
+            const data = await axios.get(graphApiUrl);
+            return data.data.data;
+        };
+        getData().then(data => {
+            const urlData = data.map(e => e.media_url);
+            console.log(urlData);
+        });
     }
-}
+};
