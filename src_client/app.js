083f29393866681f206881f5812077f6ab9453b5
hiroshi_sato <h.sato@diamondhead.tech>
2021-06-16 14:36:34 +0900

    refactor: 💡 lintと微調整
    ✅ Closes: #57

--- src_client/app.js
+++ src_client/app.js
@@ -3,9 +3,11 @@ import fetchName from './fetch-name';
 import fetchData from './fetch-data';
 import getAttendance from './get-attendance';
 import headerFunction from './header-function';
+import fetchInstagram from './get-instagram';
 
 domload();
 fetchName();
 fetchData();
 getAttendance();
 headerFunction();
+fetchInstagram();
