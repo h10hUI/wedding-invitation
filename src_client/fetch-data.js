bf391b8c18da768801199fdd374beb0f2ce6d632
hiroshi_sato <h.sato@diamondhead.tech>
2021-06-16 12:37:50 +0900

    style: 💄 lintをかける
    ✅ Closes: #60

--- src_client/fetch-data.js
+++ src_client/fetch-data.js
@@ -1,53 +1,61 @@
 export default function fetchData() {
-    // URL取得
-    const url = new URL(window.location.href);
-    // URLSearchParamsオブジェクト取得
-    const params = url.searchParams;
-    // ターゲットになる要素
-    const targetElement = document.querySelector('.bl_replyForm');
-    const areas = {
-        group:     document.querySelector('[data-pass-text="group"]'),
-        name:      document.querySelector('[data-pass-text="name"]'),
-        start:     document.querySelector('[data-pass-text="start"]'),
-        end:       document.querySelector('[data-pass-text="end"]'),
-        kana:      document.querySelector('[data-form-input="kana"]'),
-        first:     document.querySelector('[data-date-text="first-name"]'),
-        startTime: document.querySelector('[data-date-text="start-time"]'),
-        endTime:   document.querySelector('[data-date-text="end-time"]'),
-    };
-    // API問い合わせ先URL
-    const apiURL = 'https://script.google.com/a/hi0711.xyz/macros/s/AKfycbze5J3J7RCrD-T8meouPAKmjQn0DbOKgOG_rOyQ/exec?q=' + params.get('name');
+  // URL取得
+  const url = new URL(window.location.href);
+  // URLSearchParamsオブジェクト取得
+  const params = url.searchParams;
+  // ターゲットになる要素
+  const targetElement = document.querySelector('.bl_replyForm');
+  const areas = {
+    group: document.querySelector('[data-pass-text="group"]'),
+    name: document.querySelector('[data-pass-text="name"]'),
+    start: document.querySelector('[data-pass-text="start"]'),
+    end: document.querySelector('[data-pass-text="end"]'),
+    kana: document.querySelector('[data-form-input="kana"]'),
+    first: document.querySelector('[data-date-text="first-name"]'),
+    startTime: document.querySelector('[data-date-text="start-time"]'),
+    endTime: document.querySelector('[data-date-text="end-time"]'),
+  };
+  // API問い合わせ先URL
+  const apiURL = `https://script.google.com/a/hi0711.xyz/macros/s/AKfycbze5J3J7RCrD-T8meouPAKmjQn0DbOKgOG_rOyQ/exec?q=${params.get(
+    'name',
+  )}`;
 
-    if (targetElement) {
-        function returnData() {
-            // 変数定義
-            let groupData, nameData, startData, endData, kanaData, firstData;
+  if (targetElement) {
+    // eslint-disable-next-line
+    function returnData() {
+      // 変数定義
+      let groupData;
+      let nameData;
+      let startData;
+      let endData;
+      let kanaData;
+      let firstData;
 
-            return fetch(apiURL)
-                .then((response) => response.json())
-                .then((data) => {
-                    for (let i in data) {
-                        groupData = data[i].group;
-                        nameData = data[i].en;
-                        startData = data[i].start;
-                        endData = data[i].end;
-                        kanaData = data[i].kana;
-                        firstData = data[i].firstName;
-                    }
-                })
-                .then(() => {
-                    areas.group.innerHTML = groupData || 'ERR!';
-                    areas.name.innerHTML = nameData || 'ERR!';
-                    areas.start.innerHTML = startData || 'ERR!';
-                    areas.end.innerHTML = endData || 'ERR!';
-                    areas.kana.value = kanaData || '';
-                    areas.first.innerHTML = firstData || '';
-                    areas.startTime.innerHTML = startData || 'ERR!';
-                    areas.endTime.innerHTML = endData || 'ERR!';
-                })
-        }
-
-        returnData();
+      return fetch(apiURL)
+        .then((response) => response.json())
+        .then((data) => {
+          // eslint-disable-next-line
+          for (const i in data) {
+            groupData = data[i].group;
+            nameData = data[i].en;
+            startData = data[i].start;
+            endData = data[i].end;
+            kanaData = data[i].kana;
+            firstData = data[i].firstName;
+          }
+        })
+        .then(() => {
+          areas.group.innerHTML = groupData || 'ERR!';
+          areas.name.innerHTML = nameData || 'ERR!';
+          areas.start.innerHTML = startData || 'ERR!';
+          areas.end.innerHTML = endData || 'ERR!';
+          areas.kana.value = kanaData || '';
+          areas.first.innerHTML = firstData || '';
+          areas.startTime.innerHTML = startData || 'ERR!';
+          areas.endTime.innerHTML = endData || 'ERR!';
+        });
     }
-}
 
+    returnData();
+  }
+}
--- src_client/header-function.js
+++ src_client/header-function.js
@@ -1,55 +1,55 @@
 export default function headerFunction() {
-    const header = document.querySelector('header');
-    const list = document.querySelector('.bl_spMenu');
+  const header = document.querySelector('header');
+  const list = document.querySelector('.bl_spMenu');
 
-    if (header) {
-        // headerの高さを取得して、要素の位置を調整する
-        window.addEventListener('DOMContentLoaded', () => {
-            const headerHeight = header.clientHeight;
-            const nextElm = document.querySelector('.bl_mv');
-            nextElm.style.setProperty('margin-top', headerHeight + 'px');
-            list.style.setProperty('margin-top', headerHeight + 'px');
-        })
+  if (header) {
+    // headerの高さを取得して、要素の位置を調整する
+    window.addEventListener('DOMContentLoaded', () => {
+      const headerHeight = header.clientHeight;
+      const nextElm = document.querySelector('.bl_mv');
+      nextElm.style.setProperty('margin-top', `${headerHeight}px`);
+      list.style.setProperty('margin-top', `${headerHeight}px`);
+    });
 
-        // スクロールに合わせて、headerの表示・非表示を切り替える
-        let position = 0;
+    // スクロールに合わせて、headerの表示・非表示を切り替える
+    let position = 0;
 
-        window.addEventListener('scroll', () => {
-            const scroll = window.scrollY;
+    window.addEventListener('scroll', () => {
+      const scroll = window.scrollY;
 
-            if (scroll > 100 && scroll < position) {
-                header.setAttribute('data-header-state', 'show')
-            } else if (scroll > 100 && scroll > position) {
-                header.setAttribute('data-header-state', 'hide')
-            } else {
-                header.setAttribute('data-header-state', 'show')
-            }
+      if (scroll > 100 && scroll < position) {
+        header.setAttribute('data-header-state', 'show');
+      } else if (scroll > 100 && scroll > position) {
+        header.setAttribute('data-header-state', 'hide');
+      } else {
+        header.setAttribute('data-header-state', 'show');
+      }
 
-            position = scroll;
-        });
-    }
+      position = scroll;
+    });
+  }
 
-    // sp用のメニュー表示
-    const btn = document.querySelector('.bl_headerMenu_spMenu');
-    const elms = document.querySelectorAll('.el_spMenu_link');
+  // sp用のメニュー表示
+  const btn = document.querySelector('.bl_headerMenu_spMenu');
+  const elms = document.querySelectorAll('.el_spMenu_link');
 
-    if (btn) {
-        btn.addEventListener('click', () => {
-            const attr = list.getAttribute('data-sp-menu');
-            if (attr === 'show') {
-                list.setAttribute('data-sp-menu', 'hide');
-                btn.innerHTML = 'menu';
-            } else {
-                list.setAttribute('data-sp-menu', 'show');
-                btn.innerHTML = 'close';
-            }
-        });
+  if (btn) {
+    btn.addEventListener('click', () => {
+      const attr = list.getAttribute('data-sp-menu');
+      if (attr === 'show') {
+        list.setAttribute('data-sp-menu', 'hide');
+        btn.innerHTML = 'menu';
+      } else {
+        list.setAttribute('data-sp-menu', 'show');
+        btn.innerHTML = 'close';
+      }
+    });
 
-        Array.prototype.forEach.call(elms, elm => {
-            elm.addEventListener('click', () => {
-                list.setAttribute('data-sp-menu', 'hide')
-                btn.innerHTML = 'menu';
-            });
-        });
-    }
+    Array.prototype.forEach.call(elms, (elm) => {
+      elm.addEventListener('click', () => {
+        list.setAttribute('data-sp-menu', 'hide');
+        btn.innerHTML = 'menu';
+      });
+    });
+  }
 }
