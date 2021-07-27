1b65da4828d1854c71973a3cbda58619a305503f
hi0711 <admin@hi0711.xyz>
2021-07-27 16:31:25 +0900

    feat: 🎸 条件分岐追加
    ✅ Closes: #87

--- src_client/gallery.js
+++ src_client/gallery.js
@@ -1,31 +1,34 @@
 import Vue from 'vue';
 import axios from 'axios';
+import hasParams from './has-params';
 
 const dataUrl = process.env.GET_GALLERY_KEY;
 
 const gallery = () => {
-  const imageData = async () => {
-    const data = await axios.get(dataUrl);
+  if (hasParams().get('name')) {
+    const imageData = async () => {
+      const data = await axios.get(dataUrl);
 
-    return data.data;
-  };
+      return data.data;
+    };
 
-  imageData().then((data) => {
-    /**
-     * 配列を作り直す
-     *
-     * @return Array srcUrl, alt
-     */
-    const urlData = data.map((e) => ({
-      srcUrl: e.src_url,
-      alt: e.title,
-    }));
-    console.table(urlData);
-  });
+    imageData().then((data) => {
+      /**
+       * 配列を作り直す
+       *
+       * @return Array srcUrl, alt
+       */
+      const urlData = data.map((e) => ({
+        srcUrl: e.src_url,
+        alt: e.title,
+      }));
+      console.table(urlData);
+    });
 
-  Vue.component('hello-component', {
-    template: '<p>hello world</p>',
-  });
+    Vue.component('hello-component', {
+      template: '<p>hello world</p>',
+    });
+  }
 };
 
 export default gallery;
