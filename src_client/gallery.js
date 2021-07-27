17e1284e0f9cea495f25d746897917dcecc1db28
hi0711 <admin@hi0711.xyz>
2021-07-27 16:10:15 +0900

    feat(API): 🎸 apiからデータ取得まで
    ✅ Closes: #87

--- src_client/gallery.js
+++ src_client/gallery.js
@@ -1,6 +1,19 @@
 import Vue from 'vue';
+import axios from 'axios';
+
+const dataUrl = process.env.GET_GALLERY_KEY;
 
 const gallery = () => {
+  const imageData = async () => {
+    const data = await axios.get(dataUrl);
+
+    return data.data;
+  };
+
+  imageData().then((data) => {
+    console.table(data);
+  });
+
   Vue.component('hello-component', {
     template: '<p>hello world</p>',
   });
